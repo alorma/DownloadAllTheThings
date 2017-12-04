aab32368142a4b8d55ca5fdcb57dceda81e0cd0a
Bernat Borrás <bernatbor15@gmail.com>
2017-12-04 11:05:27 +0100

    Add repo downlaod URL

--- app/src/main/java/com/alorma/downloadallthethings/MainActivity.kt
+++ app/src/main/java/com/alorma/downloadallthethings/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        val DOWNLOAD_URL: String = "https://github.com/alorma/DownloadAllTheThings/archive/master.zip"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
