d9565581e9fad9d25426c6f2de23d92f959d707f
Bernat Borrás <bernatbor15@gmail.com>
2017-12-04 14:49:53 +0100

    No need permission, app external storage

--- app/src/main/java/com/alorma/downloadallthethings/MainActivity.kt
+++ app/src/main/java/com/alorma/downloadallthethings/MainActivity.kt
@@ -1,28 +1,21 @@
 package com.alorma.downloadallthethings
 
-import android.Manifest
 import android.app.DownloadManager
 import android.content.Context
 import android.content.IntentFilter
-import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        private val permissionRequest: Int = 1121
-
         val DOWNLOAD_URL = DownloadItem("https://github.com/alorma/DownloadAllTheThings/archive/master.zip", "Repo", "repo.zip")
         val IMAGE_URL = DownloadItem("https://avatars2.githubusercontent.com/u/887462?s=460&v=4", "Image", "avatar.jpg")
     }
 
-    private var itemToDownload: DownloadItem? = null
     private val broadcast = DownloadCompleteBroadcast()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,68 +33,35 @@ class MainActivity : AppCompatActivity() {
 
     private fun buildButtons() {
         downloadZip.setOnClickListener {
-            if (hasPermission()) {
-                download(DOWNLOAD_URL)
-            } else {
-                requestPermission(DOWNLOAD_URL)
-            }
+            download(DOWNLOAD_URL)
         }
 
         downloadImage.setOnClickListener {
-            if (hasPermission()) {
-                download(IMAGE_URL)
-            } else {
-                requestPermission(IMAGE_URL)
-            }
+            download(IMAGE_URL)
         }
 
         downloadAll.setOnClickListener {
-            if (hasPermission()) {
-                download(DOWNLOAD_URL)
-                download(IMAGE_URL)
-            } else {
-                requestPermission()
-            }
+            download(DOWNLOAD_URL)
+            download(IMAGE_URL)
         }
     }
 
     private fun download(item: DownloadItem) {
         val downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
 
+        val externalCacheDir = externalCacheDir
+
+        val file = File(externalCacheDir.absolutePath + "/downloads/${item.filename}")
+
         val request = DownloadManager.Request(Uri.parse(item.url)).apply {
             setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
             setTitle(item.title)
             setDescription("Downloading ${item.title}")
-            setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "/DownloadAllTheThings/${item.filename}")
+            setDestinationUri(Uri.fromFile(file))
         }
 
         val refId = downloadManager.enqueue(request)
         broadcast.addDownload(refId, item)
-    }
-
-    private fun hasPermission(): Boolean =
-            ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-
-    private fun requestPermission(item: DownloadItem? = null) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            itemToDownload = item
-            requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), permissionRequest)
-        } else {
-            item?.let {
-                download(it)
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == permissionRequest) {
-            if (grantResults.isNotEmpty()) {
-                itemToDownload?.let {
-                    download(it)
-                }
-            }
-        }
     }
 
     override fun onDestroy() {
