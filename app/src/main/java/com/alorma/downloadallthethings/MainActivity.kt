f16173d43d5c4d8196788aff63b60118c5ebf896
Bernat Borrás <bernatbor15@gmail.com>
2017-12-04 11:25:20 +0100

    Fix crash -> Add permissions

--- app/src/main/java/com/alorma/downloadallthethings/MainActivity.kt
+++ app/src/main/java/com/alorma/downloadallthethings/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.alorma.downloadallthethings
 
+import android.Manifest
 import android.app.DownloadManager
 import android.content.Context
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,20 +16,32 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     companion object {
+        private val permissionRequest: Int = 1121
+
         val DOWNLOAD_URL = DownloadItem("https://github.com/alorma/DownloadAllTheThings/archive/master.zip", "Repo", "repo.zip")
         val IMAGE_URL = DownloadItem("https://avatars2.githubusercontent.com/u/887462?s=460&v=4", "Image", "avatar.jpg")
     }
+
+    private var itemToDownload: DownloadItem? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         downloadZip.setOnClickListener {
-            download(DOWNLOAD_URL)
+            if (hasPermission()) {
+                download(DOWNLOAD_URL)
+            } else {
+                requestPermission(DOWNLOAD_URL)
+            }
         }
 
         downloadImage.setOnClickListener {
-            download(IMAGE_URL)
+            if (hasPermission()) {
+                download(IMAGE_URL)
+            } else {
+                requestPermission(IMAGE_URL)
+            }
         }
     }
 
@@ -39,5 +55,28 @@ class MainActivity : AppCompatActivity() {
         request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "/DownloadAllTheThings/${item.filename}")
 
         downloadManager.enqueue(request)
+    }
+
+    private fun hasPermission(): Boolean =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestPermission(item: DownloadItem) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            itemToDownload = item;
+            requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), permissionRequest)
+        } else {
+            download(item)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == permissionRequest) {
+            if (grantResults.isNotEmpty()) {
+                itemToDownload?.let {
+                    download(it)
+                }
+            }
+        }
     }
 }
