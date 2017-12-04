14b52a2131184e7342c881096044b9b8897fbe7a
Bernat Borrás <bernatbor15@gmail.com>
2017-12-04 11:17:57 +0100

    Enqueue download -> crash

---
+++ app/src/main/java/com/alorma/downloadallthethings/DownloadItem.kt
@@ -0,0 +1,3 @@
+package com.alorma.downloadallthethings
+
+data class DownloadItem(val url: String, val title: String, val filename: String)
--- app/src/main/java/com/alorma/downloadallthethings/MainActivity.kt
+++ app/src/main/java/com/alorma/downloadallthethings/MainActivity.kt
@@ -1,19 +1,43 @@
 package com.alorma.downloadallthethings
 
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        val DOWNLOAD_URL: String = "https://github.com/alorma/DownloadAllTheThings/archive/master.zip"
-        val IMAGE_URL: String = "https://avatars2.githubusercontent.com/u/887462?s=460&v=4"
+        val DOWNLOAD_URL = DownloadItem("https://github.com/alorma/DownloadAllTheThings/archive/master.zip", "Repo", "repo.zip")
+        val IMAGE_URL = DownloadItem("https://avatars2.githubusercontent.com/u/887462?s=460&v=4", "Image", "avatar.jpg")
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        downloadZip.setOnClickListener {
+            download(DOWNLOAD_URL)
+        }
 
+        downloadImage.setOnClickListener {
+            download(IMAGE_URL)
+        }
+    }
+
+    private fun download(item: DownloadItem) {
+        val downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+
+        val request = DownloadManager.Request(Uri.parse(item.url))
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+        request.setTitle(item.title)
+        request.setDescription("Downloading ${item.title}")
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "/DownloadAllTheThings/${item.filename}")
+
+        downloadManager.enqueue(request)
     }
 }
