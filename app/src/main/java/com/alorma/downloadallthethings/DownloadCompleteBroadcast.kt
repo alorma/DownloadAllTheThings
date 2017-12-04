a4d0ca120718eb9d7d370063115deef36c685829
Bernat Borrás <bernatbor15@gmail.com>
2017-12-04 12:29:43 +0100

    Notifications enabled android O

--- app/src/main/java/com/alorma/downloadallthethings/DownloadCompleteBroadcast.kt
+++ app/src/main/java/com/alorma/downloadallthethings/DownloadCompleteBroadcast.kt
@@ -1,12 +1,14 @@
 package com.alorma.downloadallthethings
 
 import android.app.DownloadManager
+import android.app.Notification
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.support.v4.app.NotificationCompat
-
 
 class DownloadCompleteBroadcast(private val retrieve: (Long) -> DownloadItem?,
                                 private val removeItem: (Long) -> Unit) : BroadcastReceiver() {
@@ -19,12 +21,23 @@ class DownloadCompleteBroadcast(private val retrieve: (Long) -> DownloadItem?,
     }
 
     private fun showNotification(context: Context, refId: Long, downloadItem: DownloadItem) {
+
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel("Downloads", "Downloads", NotificationManager.IMPORTANCE_DEFAULT).apply {
+                enableLights(true)
+                enableVibration(true)
+                setShowBadge(true)
+                lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+            }
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
         val mBuilder = NotificationCompat.Builder(context, "Downloads")
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(context.getString(R.string.app_name))
                 .setContentText("Download ${downloadItem.title} completed")
 
-        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(refId.toInt(), mBuilder.build())
 
         removeItem.invoke(refId)
