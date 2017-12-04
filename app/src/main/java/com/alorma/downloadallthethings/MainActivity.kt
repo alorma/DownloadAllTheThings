61161c0840074b9263f657f33f36975b82084f77
Bernat Borrás <bernatbor15@gmail.com>
2017-12-04 11:05:58 +0100

    Add alorma avatar downlaod URL

--- app/src/main/java/com/alorma/downloadallthethings/MainActivity.kt
+++ app/src/main/java/com/alorma/downloadallthethings/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.alorma.downloadallthethings
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         val DOWNLOAD_URL: String = "https://github.com/alorma/DownloadAllTheThings/archive/master.zip"
+        val IMAGE_URL: String = "https://avatars2.githubusercontent.com/u/887462?s=460&v=4"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
